f9400c352c4ca118fac9721edcc7b67ada179340
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-09-15 21:23:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-st.title("title")
-st.header("header")
-st.markdown
-st.write
-
-
-for i in range(1,100)
-range.random
-print(i)
+st.title("AI Demo with Streamlit")
+name = st.text_input("请输入你的名字")
+if st.button("打招呼"):
+    st.write(f"你好，{name}！这是一个Streamlit示例。")
