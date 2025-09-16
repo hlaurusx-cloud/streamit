363c805ccae12eba29091bae8f80e510ae65ef4a
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-09-16 16:41:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ st.header("입력")
 col1, col2 = st.columns(2)
 
 with col1:
-    A = st.number_input("숫자 A", value=5.0, format="%.6f", step=1.0)
+    A = st.number_input("숫자 A", value=5.0, step=1.0)
 with col2:
-    B = st.number_input("숫자 B", value=3.0, format="%.6f", step=1.0)
+    B = st.number_input("숫자 B", value=3.0, step=1.0)
 
 # --- 第一项：两数相加 ---
 st.header("selectbox")
 col1, col2 = st.columns(2)
 
+with col1: 
+    
 if st.button("计算 A + B"):
     sum_ab = A + B
     st.success(f"A + B = {sum_ab}")
