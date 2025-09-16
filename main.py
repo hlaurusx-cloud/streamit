c27c7f8706e4a833def50f06a1651288d36b14e9
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-09-16 16:52:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("웹 애플리케이션")
 
-st.header("입력")
+st.header(" 값 입력")
 col1, col2 = st.columns(2)
 
 with col1:
@@ -11,5 +11,7 @@ with col2:
     B = st.number_input("숫자 B", value=3.0,  format="%.6f",step=1.0)
 
 # --- 第一项：两数相加 ---
-st.header("selectbox")
-col1, col2 = st.columns(2)
+st.header("두 값이 더하기")
+if st.button("A + B"):
+    sum = A + B
+    st.sucess(f"A + B = sum")
