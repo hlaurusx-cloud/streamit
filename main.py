a84c5328db1b12e36608a73ae146ba74403fffb7
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-09-16 16:29:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ st.header("입력")
 col1, col2 = st.columns(2)
 
 with col1:
-    A = st.number_input("입력 숫자 A", value=5.0, format="%.6f", step=1.0)
+    A = st.number_input("숫자 A", value=5.0, format="%.6f", step=1.0)
 with col2:
-    B = st.number_input("입력 숫자 B", value=3.0, format="%.6f", step=1.0)
+    B = st.number_input("숫자 B", value=3.0, format="%.6f", step=1.0)
 
 # --- 第一项：两数相加 ---
 st.header("两数相加")
