74285277cf63c34b2def8544d1f60b3095b0ea5e
hlaurusx-cloud <h_laurus_x@pusan.ac.kr>
2025-09-16 16:28:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title("웹 애플리케이션")
 
-st.header("输入")
+st.header("입력")
 col1, col2 = st.columns(2)
 
 with col1:
-    A = st.number_input("输入数字 A", value=5.0, format="%.6f", step=1.0)
+    A = st.number_input("입력 숫자 A", value=5.0, format="%.6f", step=1.0)
 with col2:
-    B = st.number_input("输入数字 B", value=3.0, format="%.6f", step=1.0)
+    B = st.number_input("입력 숫자 B", value=3.0, format="%.6f", step=1.0)
 
 # --- 第一项：两数相加 ---
 st.header("两数相加")
